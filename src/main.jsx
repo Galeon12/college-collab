b1fc0908b1408bc5414516ca88d49b90cae5d85c
Chandan Saraswat <chandansaraswat16@gmail.com>
2026-07-12 17:59:04 +0530

    fix: resolve requestStorageAccess and COOP issues by setting cacheLocation to localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
+      cacheLocation="localstorage"
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
